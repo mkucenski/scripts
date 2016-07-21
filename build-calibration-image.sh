c8084c6acfd265628450b426f04d0457e675b6de
Matt Kucenski <matthew.kucenski@doe.gov>
2016-07-21 15:47:08 -0600

    change to use common blocksize script

--- build-calibration-image.sh
+++ build-calibration-image.sh
@@ -11,17 +11,7 @@ LOG="$LOGDIR/$SERIAL-calibration.log"
 SECTORS=`diskutil info $DEVICE | grep "Total Size:" | gsed -r 's/.+\(exactly ([[:digit:]]+) 512-Byte-Units\)/\1/'`
 SECTOR_SIZE=512
 SIZE=$(expr $SECTORS \* $SECTOR_SIZE)
-
-BS=2
-MAXBS=16384
-TEST=$BS
-while [ $(expr $SIZE % $TEST) -eq 0 ]; do
-	BS=$TEST
-	TEST=$(expr $TEST \* 2)
-done
-if [ $BS -gt $MAXBS ]; then
-	BS=$MAXBS
-fi
+BS=$($(dirname "$0")/blocksize.sh "$DEVICE")
 
 COUNT=$(expr $SIZE / $BS)
 
