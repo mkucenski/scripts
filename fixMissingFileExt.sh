d585d923e02a22bff4f35731b56b00744932879a
Matt Kucenski <matthew.kucenski@doe.gov>
2016-07-21 15:15:09 -0600

    misc

--- fixMissingFileExt.sh
+++ fixMissingFileExt.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-
 for X in "$@"; do
-	OLDEXT=`echo "$X" | grep "\." | gsed -r 's/^(.*)\.([^\.]+)$/\2/'`
-	NEWEXT=`file --extension -b "$X" | gsed -r 's/^([^\/]+).*$/\1/'`
-	DESC=`file -b "$X"`
-	if [[ -z $OLDEXT ]]; then
-		if [[ -z $NEWEXT ]]; then
-			if [[ -n `echo "$DESC" | grep "GIF"` ]]; then
-				NEWEXT="gif"
-			elif [[ -n `echo "$DESC" | grep "Bitmap"` ]]; then
-				NEWEXT="bmp"
-			fi
-		fi
+	OLDEXT=`echo "$X" | egrep '.+\..{1,4}$' | gsed -r 's/^.+\.(.{1,4})$/\1/'`
+	DESC=`file --brief "$X"`
 
-		if [[ -n $NEWEXT ]]; then
+	NEWEXT=""
+	if [[ -n $DESC ]]; then
+		if [[ -n `echo "$DESC" | grep "GIF image data"` ]]; then
+			NEWEXT="gif"
+		elif [[ -n `echo "$DESC" | grep "HTML document"` ]]; then
+			NEWEXT="html"
+		elif [[ -n `echo "$DESC" | grep "ASCII text"` ]]; then
+			NEWEXT="txt"
+		elif [[ -n `echo "$DESC" | grep "JPEG image data"` ]]; then
+			NEWEXT="jpg"
+		elif [[ -n `echo "$DESC" | grep "M3U playlist"` ]]; then
+			NEWEXT="m3u"
+		elif [[ -n `echo "$DESC" | grep "PNG image data"` ]]; then
+			NEWEXT="png"
+		elif [[ -n `echo "$DESC" | grep "XML 1.0 document"` ]]; then
+			NEWEXT="png"
+		else
+			echo "Unknown extension for <$DESC>!"
+		fi
+	else
+		echo "Missing file description!"
+	fi
+
+	if [[ -n $NEWEXT ]]; then
+		if [[ -z $OLDEXT ]]; then
 			mv -i -v "$X" "$X.$NEWEXT"
+		else
+			echo "File already has extension '$OLDEXT'!"
 		fi
 	fi
 done
