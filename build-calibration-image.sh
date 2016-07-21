00c0a406ae421506bad09fea86dcd15e744e85c4
Matt Kucenski <matthew.kucenski@doe.gov>
2016-07-21 15:14:37 -0600

    adding rudimentary method of finding/using a larger BS that still covers the whole device

--- build-calibration-image.sh
+++ build-calibration-image.sh
@@ -1,43 +1,65 @@
 #!/bin/bash
 
-DEVICE="$1"
-SECTORS="$2"
-SERIAL="$3"
-LOG="$4"
+# diskutil info [disk] -> sectors
 
+DEVICE="$1"
+SERIAL="$2"
+LOGDIR="$3"
+
+LOG="$LOGDIR/$SERIAL-calibration.log"
+
+SECTORS=`diskutil info $DEVICE | grep "Total Size:" | gsed -r 's/.+\(exactly ([[:digit:]]+) 512-Byte-Units\)/\1/'`
 SECTOR_SIZE=512
 SIZE=$(expr $SECTORS \* $SECTOR_SIZE)
-BS=$(expr $SECTOR_SIZE \* 8)
+
+BS=2
+MAXBS=16384
+TEST=$BS
+while [ $(expr $SIZE % $TEST) -eq 0 ]; do
+	BS=$TEST
+	TEST=$(expr $TEST \* 2)
+done
+if [ $BS -gt $MAXBS ]; then
+	BS=$MAXBS
+fi
+
 COUNT=$(expr $SIZE / $BS)
 
-echo "Building Calibration Drive ($1)..." | tee "$4"
+PATTERN="The quick brown fox jumps over the lazy dog!"
 
-echo "Device: $DEVICE" | tee -a "$4"
-echo "512-byte Sectors: $SECTORS" | tee -a "$4"
-echo "Disk Size: $SIZE" | tee -a "$4"
-echo "Block Size: $BS" | tee -a "$4"
-echo "Block Count: $COUNT" | tee -a "$4"
+date "+%Y%m%d" | tee -a "$LOG"
 
-PATTERN="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890abcdefghijklmnopqrstuvwxyz01234567890"
+echo "Building Calibration Drive ($DEVICE)..." | tee -a "$LOG"
 
-echo "" | tee -a "$4"
-echo "Building expected MD5..." | tee -a "$4"
-EXPECTED_MD5=`yes $PATTERN | dd bs=$BS count=$COUNT | openssl md5 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
-echo "$EXPECTED_MD5 - MD5 Expected from Pattern Generation" | tee -a "$4"
-EXPECTED_SHA1=`yes $PATTERN | dd bs=$BS count=$COUNT | openssl sha1 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
-echo "$EXPECTED_SHA1 - SHA1 Expected from Pattern Generation" | tee -a "$4"
+echo "Device: $DEVICE" | tee -a "$LOG"
+echo "512-byte Sectors: $SECTORS" | tee -a "$LOG"
+echo "Disk Size: $SIZE" | tee -a "$LOG"
+echo "Block Size: $BS" | tee -a "$LOG"
+echo "Block Count: $COUNT" | tee -a "$LOG"
+echo "Pattern: '$PATTERN'"
 
-echo "" | tee -a "$4"
-echo "Writing pattern to device ($DEVICE)..." | tee -a "$4"
-yes $PATTERN | dd bs=$BS count=$COUNT of="$DEVICE"
+echo "" | tee -a "$LOG"
+echo "Building expected MD5..." | tee -a "$LOG"
+EXPECTED_MD5=`yes "$PATTERN" | dd bs=$BS count=$COUNT 2>> "$LOG" | openssl md5 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
+echo "$EXPECTED_MD5 - MD5 Expected from Pattern Generation" | tee -a "$LOG"
+#EXPECTED_SHA1=`yes "$PATTERN" | dd bs=$BS count=$COUNT 2>> "$LOG" | openssl sha1 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
+#echo "$EXPECTED_SHA1 - SHA1 Expected from Pattern Generation" | tee -a "$LOG"
 
-echo "" | tee -a "$4"
-echo "Reading from device ($DEVICE)..." | tee -a "$4"
-DEVICE_MD5=`dd bs=$BS count=$COUNT if="$DEVICE" | openssl md5 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
-echo "$DEVICE_MD5 - MD5 Reported by Device ($DEVICE)" | tee -a "$4"
+echo "" | tee -a "$LOG"
+echo "Writing pattern to device ($DEVICE)..." | tee -a "$LOG"
+yes "$PATTERN" | dd bs=$BS of="$DEVICE" 2>> "$LOG"
 
-echo "" | tee -a "$4"
-echo "$DEVICE_MD5 - MD5 Reported by Device ($DEVICE)" | tee -a "$4"
-echo "$EXPECTED_MD5 - MD5 Expected from Pattern Generation" | tee -a "$4"
-echo "$EXPECTED_SHA1 - SHA1 Expected from Pattern Generation" | tee -a "$4"
+echo "" | tee -a "$LOG"
+echo "Reading from device ($DEVICE)..." | tee -a "$LOG"
+DEVICE_MD5=`dd bs=$BS if="$DEVICE" 2>> "$LOG" | openssl md5 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
+echo "$DEVICE_MD5 - MD5 Reported by Device ($DEVICE)" | tee -a "$LOG"
+#DEVICE_SHA1=`dd bs=$BS if="$DEVICE" 2>> "$LOG" | openssl sha1 | tr a-z A-Z | gsed -r 's/\(STDIN\)= //'`
+#echo "$DEVICE_SHA1 - SHA1 Reported by Device ($DEVICE)" | tee -a "$LOG"
+
+echo "" | tee -a "$LOG"
+echo "$DEVICE_MD5 - MD5 Reported by Device ($DEVICE)" | tee -a "$LOG"
+echo "$EXPECTED_MD5 - MD5 Expected from Pattern Generation" | tee -a "$LOG"
+#echo "$EXPECTED_SHA1 - SHA1 Expected from Pattern Generation" | tee -a "$LOG"
+
+echo "" | tee -a "$LOG"
 
