c8d0fbcf435e6c7cd8883545a6692b179e97c250
Matt Kucenski <matthew.kucenski@doe.gov>
2016-07-22 10:53:45 -0600

    ...

--- updateMacPorts.sh
+++ updateMacPorts.sh
@@ -3,12 +3,12 @@
 LOG=~/Logs/installed_ports.log
 
 date "+%Y%m%d" >> "$LOG"
-echo "Updating all port system and all outdated, previously installed ports...."
+echo "Updating all port system and all outdated, previously installed ports...." | tee -a "$LOG"
 port installed >> "$LOG" 
-port -d selfupdate | tee -a "$LOG"
+port -d selfupdate 2>&1 | tee -a "$LOG"
 port fetch outdated
 port -ucp upgrade outdated | tee -a "$LOG"
 port installed >> "$LOG"
-echo "Done with upgrade/updates!"
+echo "Done with upgrade/updates!" | tee -a "$LOG"
 date "+%Y%m%d" >> "$LOG"
 
