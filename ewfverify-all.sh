b8936fb883b139cae7fd30d3b8a63ae648b354b9
Matt Kucenski <matthew.kucenski@doe.gov>
2016-07-29 10:42:41 -0600

    cleaned up script, changed logging to report summarized info to main log; details to individual logs

--- ewfverify-all.sh
+++ ewfverify-all.sh
@@ -6,12 +6,12 @@ LOGDIR="$2"
 # Find all .E01 files within BASEDIR and execute <ewfverify> on each. Log all results
 # to <LOGDIR/ewfverify.log> as well as individual results in each image directory.
 
+ERR=0
+DATE=$(date "+%Y%m%d")
 LOGALL="$LOGDIR/ewfverify.log"
 
-VER=$(ewfverify -V | grep -v "^$")
-echo "BEGIN: `date \"+%Y%m%d\"`" >> "$LOGALL"
-echo "$VER" >> "$LOGALL"
-echo "" >> "$LOGALL"
+# Store version information for ewfverify
+VER=$(ewfverify -V | head -n 1)
 
 # Adjust field separators for for loop to support whitespace in filenames
 IFS=$(echo -en "\n\b")
@@ -19,19 +19,35 @@ IFS=$(echo -en "\n\b")
 for IMAGE in `find "$BASEDIR" -type f -iname "*.e01"`; do
 	NAME=$(basename "$IMAGE")
 	IMAGEDIR=$(cd $(dirname "$IMAGE"); pwd)
+	FULLIMAGE="$IMAGEDIR/$NAME"
 	LOG="$IMAGEDIR/ewfverify-$NAME.log"
 
-	echo "BEGIN: `date \"+%Y%m%d\"`" >> "$LOG"
+	echo "BEGIN: $DATE" >> "$LOG"
 	echo "$VER" >> "$LOG"
 	echo "" >> "$LOG"
-	echo "Image: $IMAGEDIR/$NAME" | tee -a "$LOGALL" "$LOG"
+	echo "Image: $FULLIMAGE" | tee -a "$LOG"
 
-	ewfverify -q "$IMAGE" 2>&1 | grep -v "^$" | tee -a "$LOGALL" "$LOG"
+	RESULT=$(ewfverify -q "$IMAGE" | grep -v "^$")
 
-	echo "END: `date \"+%Y%m%d\"`" >> "$LOG"
+	# Output full results to individual log file
+	echo "$RESULT" >> "$LOG"
+
+	VERIFY=$(echo "$RESULT" | egrep "ewfverify: (SUCCESS|FAILURE)" | gsed -r 's/ewfverify: (.+)/\1/')
+	if [ $VERIFY == "SUCCESS" ]; then
+		ERR=1
+	elif [ $VERIFY == "FAILURE" ]; then
+		ERR=0
+	else
+		VERIFY="UNKNOWN"
+	fi
+	echo "$VERIFY"
+
+	# Output summarized result to primary log file
+	echo "$DATE: ($VERIFY) $FULLIMAGE" >> "$LOGALL"
+
+	echo "END: $DATE" >> "$LOG"
 	echo "" >> "$LOG"
 done
 
-echo "END: `date \"+%Y%m%d\"`" >> "$LOGALL"
-echo "" >> "$LOGALL"
+exit $ERR
 
