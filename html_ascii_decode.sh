4726a3dc10206fca8816807cd54d0421158f8afa
Matt Kucenski <matthew.kucenski@doe.gov>
2016-08-02 15:55:37 -0600

    ...

---
+++ html_ascii_decode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SED=$(dirname "$0")/sed/html_ascii_encoding.sed
+cat /dev/stdin | gsed -f "$SED" | gsed -f "$SED" | gsed -f "$SED"
+
+exit 0
+
+PASSES="$1"
+
+if [ -z "$PASSES" ]; then
+	PASSES=3
+fi
+
+# Running this loop multiple times, should gradually convert all HTML-encoded ASCII back to readable ASCII
+
+for PASS in `seq 1 $PASSES`; do
+	# TODO... how do I operate multiple passes against the sed file from STDIN
+done
+
