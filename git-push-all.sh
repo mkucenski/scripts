069de2f293d7eefae818d2bf92194e4a54bf1925
Matt Kucenski <mkucenski@me.com>
2016-08-02 09:03:11 -0600

    ...

--- git-push-all.sh
+++ git-push-all.sh
@@ -8,20 +8,10 @@ BASEDIR="./"
 # Adjust field separators for for loop to support whitespace in filenames
 IFS=$(echo -en "\n\b")
 
-A="no changes added to commit"
-B="On branch master"
-C="Your branch is up-to-date"
-D="nothing to commit, working tree clean"
-E="^$"
-F="to update what will be committed"
-G="to include in what will be committed"
-H="to discard changes in working directory"
-
 for DIR in `find "$BASEDIR" -type d -depth 1`; do
 	echo "$DIR:"
 	pushd "$DIR" > /dev/null
-	git push
-	# | egrep -v "$A|$B|$C|$D|$E|$F|$G|$H"
+	git push | egrep -vi "^$"
 	popd > /dev/null
 done
 
--- git-status-all.sh
+++ git-status-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASEDIR="$1"
+BASEDIR="./"
 
 # Find all .E01 files within BASEDIR and execute <ewfverify> on each. Log all results
 # to <LOGDIR/ewfverify.log> as well as individual results in each image directory.
@@ -12,15 +12,16 @@ A="no changes added to commit"
 B="On branch master"
 C="Your branch is up-to-date"
 D="nothing to commit, working tree clean"
-E="^$"
+E="nothing to commit, working directory clean"
 F="to update what will be committed"
 G="to include in what will be committed"
 H="to discard changes in working directory"
+I="nothing added to commit but untracked files present"
 
 for DIR in `find "$BASEDIR" -type d -depth 1`; do
 	echo "$DIR:"
 	pushd "$DIR" > /dev/null
-	git status | egrep -v "$A|$B|$C|$D|$E|$F|$G|$H"
+	git status | egrep -vi "$A|$B|$C|$D|$E|$F|$G|$H|$I" | egrep -vi "^$"
 	popd > /dev/null
 done
 
