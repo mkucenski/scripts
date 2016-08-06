aa3590c15cd02a653b375cefdb42f031c8a4a4ad
Matt Kucenski <mkucenski@me.com>
2016-08-06 07:44:34 -0600

    ...

--- git-update-all.sh
+++ git-update-all.sh
@@ -8,6 +8,7 @@ IFS=$(echo -en "\n\b")
 for DIR in `find "$BASEDIR" -type d -depth 1`; do
 	echo "$DIR:"
 	pushd "$DIR" > /dev/null
+	git push | egrep -vi "^$"
 	git pull | egrep -vi "^$"
 	popd > /dev/null
 done
