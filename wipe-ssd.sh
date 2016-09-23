d3b86c2eee9dcec0b5f2595fabc6ab0adebf4ffa
Matt Kucenski <matthew.kucenski@doe.gov>
2016-09-23 15:19:59 -0600

    correcting renamed script

--- wipe-ssd.sh
+++ wipe-ssd.sh
@@ -11,7 +11,7 @@ echo "Starting SSD/Flash wipe on device ($DEVICE)..." | tee -a "$LOG"
 
 for PASS in `seq 1 $PASSES`; do
 	echo "Zero-Pass ($PASS/$PASSES)..." | tee -a "$LOG"
-	wipeDisk.sh "$DEVICE" "$LOG"
+	$(dirname "$0")/wipe-disk.sh "$DEVICE" "$LOG"
 done
 
 echo "Verifying entire wiped device ($DEVICE) using (bs=$BS)..." | tee -a "$LOG"
