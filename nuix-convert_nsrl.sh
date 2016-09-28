06e5ced3e2f7cf9bf6028cd36e90c385ce95300b
Matt Kucenski <matthew.kucenski@doe.gov>
2016-09-28 13:17:37 -0600

    ...

--- nuix-convert_nsrl.sh
+++ nuix-convert_nsrl.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 FILE="$1"
+DEST="$2"
 
-$(dirname "$0")/convert2ascii.sh "$FILE" | cut -d '"' -f 4 | sort | uniq | sed '/MD5/d'
+TEMP=$(mktemp -t $(basename "$0") || exit 1)
+TEMP2=$(mktemp -t $(basename "$0") || exit 1)
 
+# Get rid of unicode characters that cause problems
+$(dirname "$0")/convert2ascii.sh "$FILE" > "$TEMP"
+
+# Trim out only the MD5 hash value
+cut -d '"' -f 4 "$TEMP" > "$TEMP2"
+
+# Sort only the unique values and remove the 'MD5' column header line
+sort "$TEMP2" | uniq | sed '/MD5/d' > "$2"
+
+# Delete temp files
+rm "$TEMP"
+rm "$TEMP2"
+
+exit 0
