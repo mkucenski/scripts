36bff0ac263bcb398b213b7be1a5be6d0426867a
Matt Kucenski <matthew.kucenski@doe.gov>
2016-12-13 07:21:26 -0700

    ...

--- photorec.sh
+++ photorec.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Consistently run photorec for carving images
+# Wrapper to consistently run photorec for carving files
 
-# "$1" = data to carve
-# "$2" = destination
+IMAGE="$1"
+DEST="$2"
 
-photorec /version > "$2/photorec_version.txt"
-photorec /log /d "$2" "$1"
+LOG="$DEST/photorec_wrapper.log"
+
+if [ ! -e "$DEST" ]; then
+	mkdir -p "$DEST"
+fi
+
+echo "BEGIN: `date \"+%Y%m%d\"`" >> "$LOG"
+echo "CMD: '$0 $@'" >> "$LOG"
+cat "$0" | gsed -r 's/^/>>   /' >> "$LOG"
+
+photorec /version >> "$LOG" 2>> "$LOG"
+photorec /log /d "$DEST" "$IMAGE" 2>> "$LOG"
+mv photorec.* "$DEST/" 2>&1 >> "$LOG"
+
+echo "END: `date \"+%Y%m%d\"`" >> "$LOG"
 
