941fcfc8a767c9c4b0ab985950138ed42f48b673
Matt Kucenski <matthew.kucenski@doe.gov>
2016-12-19 15:27:28 -0700

    formalizing with logging

--- convert-vmdk-to-raw.sh
+++ convert-vmdk-to-raw.sh
@@ -3,5 +3,16 @@
 VMDK="$1"
 RAW="$2"
 
-qemu-img convert -f vmdk "$VMDK" -O raw "$RAW"
+DEBUG=0
+LOG="$RAW.log"
+
+if [ ! -e "$RAW" ]; then
+	qemu-img convert -f vmdk "$VMDK" -O raw "$RAW" 2>&1 | tee -a "$LOG"
+else
+	echo "$0: Destination RAW already exists!" | tee -a "$LOG"
+fi
+
+#!/bin/bash
+
+
 
