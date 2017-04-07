860f98771963ec023cf398615f9a3dd6ab6eea91
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-07 12:25:29 -0600

    minor fixes

--- wipe-verify-full.sh
+++ wipe-verify-full.sh
@@ -8,7 +8,7 @@ LOG="$3/$SERIALNUM-wipe.log"
 
 COUNT=1024
 BS=$($(dirname "$0")/blocksize.sh "$DEVICE")
-SECTORS=$($(dirname "$0")/device-sectors.sh "$DEVICE")
+SECTORS=$($(dirname "$0")/disksectors.sh "$DEVICE")
 SECTOR_SIZE=512
 SIZE=$(expr $SECTORS \* $SECTOR_SIZE)
 BLOCKS=$(expr $SIZE / $BS)
@@ -17,7 +17,7 @@ echo
 date "+%Y%m%d" >> "$LOG"
 
 echo "Verifying entire wiped device ($DEVICE) using (bs=$BS)..." | tee -a "$LOG"
-dd if="$DEVICE" bs=$BS 2>> "$LOG" | xxd -a | tee -a "$LOG"
+dd if="$DEVICE" bs=$BS | xxd -a | tee -a "$LOG"
 
 date "+%Y%m%d" >> "$LOG"
 
--- wipe-verify.sh
+++ wipe-verify.sh
@@ -8,7 +8,7 @@ LOG="$3/$SERIALNUM-wipe.log"
 
 COUNT=1024
 BS=$($(dirname "$0")/blocksize.sh "$DEVICE")
-SECTORS=$($(dirname "$0")/device-sectors.sh "$DEVICE")
+SECTORS=$($(dirname "$0")/disksectors.sh "$DEVICE")
 SECTOR_SIZE=512
 SIZE=$(expr $SECTORS \* $SECTOR_SIZE)
 BLOCKS=$(expr $SIZE / $BS)
@@ -19,15 +19,15 @@ echo "Verifying device wipe status ($DEVICE)..." | tee -a "$LOG"
 
 echo
 echo "Verifying first $(expr $BS \* $COUNT)-bytes of ($DEVICE) using (bs=$BS)..." | tee -a "$LOG"
-dd if="$DEVICE" bs=$BS count=$COUNT 2>> "$LOG" | xxd -a | tee -a "$LOG"
+dd if="$DEVICE" bs=$BS count=$COUNT | xxd -a | tee -a "$LOG"
 
 echo
 echo "Verifying middle $(expr $BS \* $COUNT)-bytes of ($DEVICE) using (bs=$BS, skip=$(expr $BLOCKS / 2 - $COUNT / 2))..." | tee -a "$LOG"
-dd if="$DEVICE" bs=$BS skip=$(expr $BLOCKS / 2 - $COUNT / 2) count=$COUNT 2>> "$LOG" | xxd -a | tee -a "$LOG"
+dd if="$DEVICE" bs=$BS skip=$(expr $BLOCKS / 2 - $COUNT / 2) count=$COUNT | xxd -a | tee -a "$LOG"
 
 echo
 echo "Verifying last $(expr $BS \* $COUNT)-bytes of ($DEVICE) using (bs=$BS, skip=$(expr $BLOCKS - $COUNT))..." | tee -a "$LOG"
-dd if="$DEVICE" bs=$BS skip=$(expr $BLOCKS - $COUNT) count=$COUNT 2>> "$LOG" | xxd -a | tee -a "$LOG"
+dd if="$DEVICE" bs=$BS skip=$(expr $BLOCKS - $COUNT) count=$COUNT | xxd -a | tee -a "$LOG"
 
 date "+%Y%m%d" >> "$LOG"
 
