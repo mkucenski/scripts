4e919f0737ad726063dd85873be00763337ea930
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-13 07:19:51 -0600

    swapping slashes to match Windows

--- ditto_xml-to-fciv.sh
+++ ditto_xml-to-fciv.sh
@@ -8,7 +8,7 @@ KEY="1.75"
 
 if [ -e "$XML" ]; then
 	$(dirname "$0")/fciv.sh
-	grep "^<entry>" "$XML" | grep "<md5>" | gsed -r 's/^.+<name>(.+)<\/name>.+<md5>(.+)<\/md5>.+$/\2 0000000000000000000000000000000000000000 .\\\\\1/' | sort --key=$KEY
+	grep "^<entry>" "$XML" | grep "<md5>" | gsed -r 's/^.+<name>(.+)<\/name>.+<md5>(.+)<\/md5>.+$/\2 0000000000000000000000000000000000000000 .\\\\\1/; s/\//\\/g' | sort --key=$KEY
 else
 	echo "Error! Unable to find file!" > /dev/stderr
 fi
