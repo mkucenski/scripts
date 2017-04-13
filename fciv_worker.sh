64ef81ffffadda24a98e3c744db29b046479bac8
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-13 13:28:33 -0600

    stderr output of filename as processing to make it easier to see what is going on.

--- fciv_worker.sh
+++ fciv_worker.sh
@@ -4,7 +4,9 @@ FILE="$1"
 SHA1="$2"
 
 if [ "$SHA1" != "0" ]; then
+	echo "$FILE" > /dev/stderr
 	echo `openssl dgst -md5 -r "$FILE" | gsed -r 's/(^.+) \*.*/\1/'` `openssl dgst -sha1 -r "$FILE" | gsed -r 's/\*//;s/\//\\\/g'`
 else
+	echo "$FILE" > /dev/stderr
 	echo `openssl dgst -md5 -r "$FILE" | gsed -r 's/(^.+) \*.*/\1/'` `echo "0000000000000000000000000000000000000000 *$FILE" | gsed -r 's/\*//;s/\//\\\/g'`
 fi
