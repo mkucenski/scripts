691f5fa929fc7c9f741d8f7f68cc1821b0e71eba
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-13 07:20:58 -0600

    added ability to skip sha1 calculation; works with comparison to dittodx script that doesn't generate sha1

--- fciv_recursive.sh
+++ fciv_recursive.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 DIR="$1"
+SHA1="$2"
+
 KEY="1.75"
 
 if [ -e "$DIR" ]; then
 	$(dirname "$0")/fciv.sh
-	find "$DIR" -type f -exec $(dirname "$0")/fciv_worker.sh {} \; | sort --key=$KEY
+	find "$DIR" -type f -exec $(dirname "$0")/fciv_worker.sh {} $SHA1 \; | sort --key=$KEY
 else
 	echo "Error! Unable to find <$DIR>!" > /dev/stderr
 fi
--- fciv_worker.sh
+++ fciv_worker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 FILE="$1"
-echo `openssl dgst -md5 -r "$FILE" | gsed -r 's/(^.+) \*.*/\1/'` `openssl dgst -sha1 -r "$FILE" | gsed -r 's/\*//;s/\//\\\/g'`
+SHA1="$2"
+
+if [ "$SHA1" != "0" ]; then
+	echo `openssl dgst -md5 -r "$FILE" | gsed -r 's/(^.+) \*.*/\1/'` `openssl dgst -sha1 -r "$FILE" | gsed -r 's/\*//;s/\//\\\/g'`
+else
+	echo `openssl dgst -md5 -r "$FILE" | gsed -r 's/(^.+) \*.*/\1/'` `echo "0000000000000000000000000000000000000000 *$FILE" | gsed -r 's/\*//;s/\//\\\/g'`
+fi
