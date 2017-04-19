dc965f7bea3e0c2100f5a2d68c5bc539fe3416c2
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-19 08:58:16 -0600

    recovering files via sleuthkit using mactime body records

---
+++ tsk-mct-recover-files_worker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. $(dirname "$0")/common-include.sh
+
+IMAGE="$1"
+OFFSET="$2"
+DEST="$3"
+MCTENTRY="$3"
+
+if [ -n "$MCTENTRY" ]; then
+	REGEX="^[^|]+\|[^|]+\|[^|]+\|r[^|]+\|[^|]+\|[^|]+\|[^|]+\|[^|]+\|[^|]+\|[^|]+\|[^|]+$"
+	SED="^([^|]+)\|([^|]+)\|([^|]+)\|(r[^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)$"
+	FILE=$(echo "$MCTENTRY" | grep -v "\$FILE_NAME" | egrep "$REGEX" | $SEDCMD -r "s/$SED/\2/")
+	INODE=$(echo "$MCTENTRY" | grep -v "\$FILE_NAME" | egrep "$REGEX" | $SEDCMD -r "s/$SED/\3/" | egrep "[[:digit:]]+-128-[[:digit:]]+" | $SEDCMD -r "s/([[:digit:]]+)-128-[[:digit:]]/\1/")
+	if [ -n "$FILE" ]; then
+		if [ -n "$INODE" ]; then
+			mkdir -p "$DEST/$(dirname "$FILE")"
+			icat -o $OFFSET "$IMAGE" $INODE > "$DEST/$FILE"
+			echo "$FILE"
+		fi
+	fi
+else
+	ERROR "Invalid mactime entry!" "$0"
+fi
+
