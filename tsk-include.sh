6960375d19964e4047ae790adc937044933503e6
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-21 10:39:50 -0600

    ...

--- tsk-include.sh
+++ tsk-include.sh
@@ -5,10 +5,10 @@ TSK_MCT_REGEX="^[^|]+\|[^|]+\|[^|]+\|r[^|]+\|[^|]+\|[^|]+\|[^|]+\|[^|]+\|[^|]+\|
 TSK_MCT_SED="^([^|]+)\|([^|]+)\|([^|]+)\|(r[^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)$"
 
 function _tsk_mct_file() {
-	echo "$1" | grep -v "\$FILE_NAME" | egrep "$MCT_REGEX" | $SEDCMD -r "s/$MCT_SED/\2/"
+	echo "$1" | grep -v "\$FILE_NAME" | egrep "$TSK_MCT_REGEX" | $SEDCMD -r "s/$TSK_MCT_SED/\2/"
 }
 
 function _tsk_mct_inode() {
-	echo "$1" | grep -v "\$FILE_NAME" | egrep "$MCT_REGEX" | $SEDCMD -r "s/$MCT_SED/\3/" | egrep "[[:digit:]]+-128-[[:digit:]]+" | $SEDCMD -r "s/([[:digit:]]+)-128-[[:digit:]]/\1/"
+	echo "$1" | grep -v "\$FILE_NAME" | egrep "$TSK_MCT_REGEX" | $SEDCMD -r "s/$TSK_MCT_SED/\3/" | egrep "[[:digit:]]+-128-[[:digit:]]+" | $SEDCMD -r "s/([[:digit:]]+)-128-[[:digit:]]/\1/"
 }
 
