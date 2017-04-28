af19e2efa5c4405eed267434a8427826dd06ca2c
Matt Kucenski <matthew.kucenski@doe.gov>
2017-04-28 07:59:08 -0600

    ...

--- rsync.sh
+++ rsync.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+. $(dirname "$0")/common-include.sh
+. $(dirname "$0")/unison/unison-sync-inc.sh
 
 SRCDIR="$1"
 DSTBASEDIR="$2"
 
-. $(dirname "$0")/unison/unison-sync-inc.sh
-
 if [ -e "$SRCDIR" ]; then
 	if [ -e "$DSTBASEDIR" ]; then
-		execRsync2 "$SRCDIR" "$DSTBASEDIR"
+		ERR=0
+		echo "--- $SRCDIR -> $DSTBASEDIR ---"
+		RESULT=$(execRsync2 "$SRCDIR" "$DSTBASEDIR")
+		ERR=$(expr $ERR + $?)
+		if [ $ERR -ne 0 ]; then
+			echo "execRsync: $RESULT ($ERR)" > /dev/stderr
+		else
+			echo "Success!"
+		fi
 	else
 		echo "<$DSTBASEDIR> Not Available!"
 	fi
