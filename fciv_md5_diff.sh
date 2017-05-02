5d1198462668f52a22b59477280db5dc1713ed38
Matt Kucenski <matthew.kucenski@doe.gov>
2017-05-02 15:06:02 -0600

    working toward a more advanced way to find/report on difference between two fciv.md5 files.

--- fciv_md5_diff.sh
+++ fciv_md5_diff.sh
@@ -9,15 +9,31 @@ fi
 
 if [ -e "$FILE1" ]; then
 	if [ -e "$FILE2" ]; then
-		TMP1=$(mktemp)
-		TMP2=$(mktemp)
+		TMP1=$(mktemp -t $(basename "$0"))
+		TMP2=$(mktemp -t $(basename "$0"))
+
+		INFO "Isolating MD5 values..."
 		HEADER1="//"
 		HEADER2="// Open File Checksum Integrity Verifier version 1.0."
 		HEADER3="		MD5				SHA-1"
 		HEADER4="-------------------------------------------------------------------------"
 		$SEDCMD -r 's/$HEADER1//; s/$HEADER2//; s/$HEADER3//; s/$HEADER4//; s/[[:space:]]+.+//' "$FILE1" | sort -u > "$TMP1"
 		$SEDCMD -r 's/$HEADER1//; s/$HEADER2//; s/$HEADER3//; s/$HEADER4//; s/[[:space:]]+.+//' "$FILE2" | sort -u > "$TMP2"
-		diff -wiy --suppress-common-lines "$TMP1" "$TMP2"
+
+		INFO "Comparing and sorting for unique/different MD5 values..."
+		UNIQ_HASHES=$(mktemp -t $(basename "$0"))
+		diff -wi --suppress-common-lines "$TMP1" "$TMP2" | egrep "(<|>)" | gsed -r 's/[<>][[:space:]]+//g' | sort -u > "$UNIQ_HASHES"
+
+		INFO "Searching original files for unique/different MD5 values..."
+		grep -i -f "$UNIQ_HASHES" "$FILE1" > "$TMP1"
+		grep -i -f "$UNIQ_HASHES" "$FILE2" > "$TMP2"
+
+		INFO "Comparing unique/different MD5 values from each file..."
+		diff -wi --suppress-common-lines "$TMP1" "$TMP2"
+
+		rm "$TMP1"
+		rm "$TMP2"
+		rm "$UNIQ_HASHES"
 	else
 		ERROR "Invalid second file ($FILE2)!" "$0"
 	fi
