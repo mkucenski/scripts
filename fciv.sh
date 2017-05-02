f3070b1d07d70546bc0704c365234aac57049279
Matt Kucenski <matthew.kucenski@doe.gov>
2017-05-02 15:03:51 -0600

    adding USAGE functionality

--- fciv.sh
+++ fciv.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 . $(dirname "$0")/common-include.sh
 
-if [ $# -eq 0 ]; then
-	USAGE "FILES..." && exit 0
-fi
-
 echo "//"
 echo "// Open File Checksum Integrity Verifier version 1.0."
 echo "//"
