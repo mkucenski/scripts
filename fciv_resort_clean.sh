fe95a040c2af7f5e76402ebfb7c58729404a3ec0
Matt Kucenski <matthew.kucenski@doe.gov>
2017-05-03 15:24:01 -0600

    init

---
+++ fciv_resort_clean.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+. $(dirname "$0")/common-include.sh
+
+FILE="$1"
+if [ $# -ne 1 ]; then
+	USAGE "FILE" && exit 0
+fi
+
+KEY="1.75"
+TMP=$(mktemp -t $(basename "$0") || exit 1)
+
+if [ -e "$FILE" ]; then
+	BACKUP=$(mktemp "$(dirname "$FILE")/$(basename "$FILE").XXXXXX")
+	INFO "Making backup copy of original file... ($BACKUP)"
+	cp "$FILE" "$BACKUP"
+
+	INFO "Re-sorting and cleaning ($FILE) based on key ($KEY)..."
+	$SEDCMD -r 's/\/\//\//g; s/\//\\/g; s/[~@%$]/_/g' "$FILE" | sort --key=$KEY > "$TMP"
+	mv "$TMP" "$FILE"
+else
+	ERROR "Unable to find ($FILE)!" "$0"
+fi
+
+if [ -e "$TMP" ]; then
+	rm "$TMP"
+fi
+
