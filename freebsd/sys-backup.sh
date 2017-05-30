039a634f171dc4f39aeadaeb14d11295bfb3dde3
Matthew Kucenski <mkucenski@me.com>
2017-05-30 13:48:50 -0600

    ...

--- freebsd/sys-backup.sh
+++ freebsd/sys-backup.sh
@@ -1,13 +1,21 @@
 #!/usr/local/bin/bash
 . ${BASH_SOURCE%/*}/../common-include.sh || exit 1
 
-DEST="$1"
+DESTDIR="$1"
 if [ $# -eq 0 ]; then
-	USAGE "DEST" && exit $COMMON_ERROR
+	USAGE "DESTDIR" && exit $COMMON_ERROR
 fi
 
-# extract via: cd "$NEWDST" &&  bunzip2 -c "$DEST" | restore -r -f 
+RV=$COMMON_SUCCESS
+if [ -d "$DESTDIR" ]; then
+	DEST="$DESTDIR/$(hostname -s) $(DATETIME).bz2"
+	dump -L0af - / | bzip2 -c > "$DEST"
+	RV=$?
+else
+	ERROR "Invalid Destination Directory!" "$0"
+fi
 
-dump -L0af - / | bzip2 -c > "$DEST"
-exit $?
+exit $RV
+
+# extract via: cd "$NEWDST" &&  bunzip2 -c "$DEST" | restore -r -f 
 
