3591d3ff1245d77c79214c923a526d4e9888d550
Matt Kucenski <mkucenski@me.com>
2017-06-12 17:40:39 -0600

    standardizing

--- ewfverify.sh
+++ ewfverify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. ${BASH_SOURCE%/*}/common-include.sh
+. ${BASH_SOURCE%/*}/common-include.sh || exit 1
 
 # NOTE: If you receive errors regarding too many files open, use 'ulimit -n XXXX' to increase the max open file limit.
 
