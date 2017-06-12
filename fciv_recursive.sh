6d83a8e85384f8111d9bc653b7863592819e3024
Matt Kucenski <mkucenski@me.com>
2017-06-12 16:43:27 -0600

    more commonality between scripts and changing from inefficient for-loop to 'find'

--- fciv_recursive.sh
+++ fciv_recursive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. ${BASH_SOURCE%/*}/common-include.sh
+. ${BASH_SOURCE%/*}/common-include.sh || exit 1
 
 DIR="$1"
 DOSHA1="$2"
@@ -10,14 +10,16 @@ fi
 RV=$COMMON_SUCCESS
 
 KEY="1.75"
-TMP=$(mktemp -t $(basename "$0") || exit $COMMON_ERROR)
+TMP=$(MKTEMP "$0" || exit $COMMON_ERROR)
+INFO_ERR "Temp: $TMP"
 
 if [ -e "$DIR" ]; then
 	${BASH_SOURCE%/*}/fciv.sh 
-	for FILE in $(find "$DIR" -type f); do
-		${BASH_SOURCE%/*}/fciv_worker.sh "$FILE" $DOSHA1 \; >> "$TMP"
-		RV=$((RV+$?))
-	done
+	find "$DIR" -type f -exec ${BASH_SOURCE%/*}/fciv_worker.sh "{}" $DOSHA1 >> "$TMP" \;
+	# for FILE in $(find "$DIR" -type f); do
+	# 	${BASH_SOURCE%/*}/fciv_worker.sh "$FILE" $DOSHA1 >> "$TMP"
+	# 	RV=$((RV+$?))
+	# done
 	sort --key=$KEY "$TMP"
 else
 	ERROR "Unable to find ($DIR)!" "$0"
