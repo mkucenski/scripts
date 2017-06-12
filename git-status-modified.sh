7ac09b87d21ad6a8af7ddacd813159232732f3e7
Matt Kucenski <mkucenski@me.com>
2017-06-12 17:38:37 -0600

    init

---
+++ git-status-modified.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git status | grep "modified:" | gsed -r 's/[[:space:]]+modified:[[:space:]]+(.+)/\1/' | xargs ls -lrth | gsed -r 's/.*[[:digit:]]+[[:space:]][[:digit:]]+:[[:digit:]]+[[:space:]](.+)/\tmodified:   \1/'
+
