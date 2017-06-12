75cfa6c13c7588adca26dab5d9aa9af67dac5e19
Matt Kucenski <mkucenski@me.com>
2017-06-12 17:39:34 -0600

    standardizing

--- tsk-mct-recover-files_worker.sh
+++ tsk-mct-recover-files_worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-. ${BASH_SOURCE%/*}/common-include.sh
-. ${BASH_SOURCE%/*}/tsk-include.sh
+. ${BASH_SOURCE%/*}/common-include.sh || exit 1
+. ${BASH_SOURCE%/*}/tsk-include.sh || exit 1
 
 IMAGE="$1"
 OFFSET="$2"
@@ -16,12 +16,12 @@ RV=$COMMON_SUCCESS
 if [ -n "$MCTENTRY" ]; then
 	FILE=$(_tsk_mct_file "$MCTENTRY")
 	INODE=$(_tsk_mct_inode "$MCTENTRY")
+	INFO "Extracting: $FILE ($INODE)..."
 	if [ -n "$FILE" ]; then
 		if [ -n "$INODE" ]; then
 			mkdir -p "$DEST/$(dirname "$FILE")"
 			icat -o $OFFSET "$IMAGE" $INODE 2> >(tee -a "$LOGFILE" >2&) > "$DEST/$FILE"
 			RV=$((RV+$?))
-			echo "$FILE"
 		fi
 	fi
 else
