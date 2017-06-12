1c4a6dc30d3a7a424f14c7687326ba7b8fd71fff
Matt Kucenski <mkucenski@me.com>
2017-06-12 17:59:59 -0600

    test-read_write-device.sh

--- git-status-modified.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git status | grep "modified:" | gsed -r 's/[[:space:]]+modified:[[:space:]]+(.+)/\1/' | xargs ls -lrth | gsed -r 's/.*[[:digit:]]+[[:space:]][[:digit:]]+:[[:digit:]]+[[:space:]](.+)/\tmodified:   \1/'
-
