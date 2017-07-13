f4c75c96004024f46748591efc29c3ea8ed3a68f
Matt Kucenski <mkucenski@me.com>
2017-07-13 10:55:51 -0600

    ...

--- wtmp_mactime.sh
+++ wtmp_mactime.sh
@@ -6,7 +6,7 @@ SRC="$2"
 cat $WTMP | while read X; do
 	DATESTR=`echo "$X" | gsed -r 's/(........................)[[:space:]]+.*/\1/'`
 	ENTRY=`echo "$X" | gsed -r 's/(........................)[[:space:]]+(.*)/\2/' | gsed -r 's/[[:space:]]+/ /g'`
-	EPOCHDATE=`~/Documents/Scripts/convertToEpoch.sh "$DATESTR"`
+	EPOCHDATE=$(${BASH_SOURCE%/*}/date-to-epoch.sh "$DATESTR")
 	echo "0|$SRC: $ENTRY|0|----------|0|0|0|$EPOCHDATE|$EPOCHDATE|$EPOCHDATE|$EPOCHDATE"
 done
 
