f841bf950ef5b2c6764581eba0e98ebd316d0282
Matt Kucenski <mkucenski@me.com>
2017-08-03 09:38:45 -0600

    init

---
+++ sqlite-dump-csv.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+. ${BASH_SOURCE%/*}/common-include.sh || exit 1
+
+DB="$1"
+CSV="$2"
+LOGFILE="$3"
+if [ -z "$LOGFILE" ]; then
+	LOGFILE="$(STRIP_EXTENSION "$CSV").log"
+fi
+if [ $# -ne 2 ]; then
+	USAGE "DB" "CSV" "LOGFILE (optional)" && exit $COMMON_ERROR
+fi
+
+RV=$COMMON_SUCCESS
+START "$0" "$LOGFILE" "$*"
+
+IFS=$(echo -en " ")
+for TABLE in $(sqlite3 "$DB" <<!
+.tables
+!); do
+	
+sqlite3 "$DB" <<!
+.headers on
+.mode csv
+.output "$(STRIP_EXTENSION "$CSV")_$TABLE.csv"
+select * from $TABLE;
+!
+
+done
+
+END "$0" "$LOGFILE"
+exit $RV
+
