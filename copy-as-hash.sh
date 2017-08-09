072ed365cc5145addd67620d9a312c096c07f96c
Matt Kucenski <mkucenski@me.com>
2017-08-09 14:45:24 -0600

    init

---
+++ copy-as-hash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. ${BASH_SOURCE%/*}/common-include.sh || exit 1
+
+# The purpose of this script is to copy files, but rename them with their MD5 hash and the existing file extension.
+# This can be used with something like 'find ./ -type f -exec <script> {} ./uniq \;' to save all unique files in one
+# directory for analysis.
+
+FILE="$1"
+DEST="$2"
+if [ $# -ne 2 ]; then
+	USAGE "FILE" "DEST" && exit $COMMON_ERROR
+fi
+
+RV=$COMMON_SUCCESS
+
+HASH="$(md5 -r "$FILE" | gsed -r 's/([^[:space:]]+).*/\1/')"
+mkdir -p "$DEST"
+cp -v "$FILE" "$DEST/$HASH.$(SAVE_EXTENSION "$FILE")"
+
+exit $RV
+
