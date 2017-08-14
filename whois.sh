8d333eb481d0b0dc5a226593e2fa12afedeec235
Matt Kucenski <mkucenski@me.com>
2017-08-14 14:16:17 -0600

    adding ability to requery the right authority when ARIN does not report correctly

--- whois.sh
+++ whois.sh
@@ -17,12 +17,29 @@ if [ ! -e "$DEST" ]; then
 fi
 
 if [ -e "$DEST" ]; then
-	START "$0" "$DEST"
+	START "$0" "$DEST" "$*"
 	INFO "$SITE -> $DEST"
 	LOG "Whois Query for: $SITE" "$DEST"
 
 	whois "$SITE" | egrep -v "^$" | egrep -v "^#" >> "$DEST"
 	RV=$?
+
+	ORG="$(grep -i "OrgName" "$DEST" | $SEDCMD -r 's/OrgName:[[:space:]]+(.+)/\1/')"
+	if [ -n "$ORG" ]; then
+		if [ "$ORG" == "Asia Pacific Network Information Centre" ]; then
+			SERVER="whois.apnic.net"
+		elif [ "$ORG" == "RIPE Network Coordination Centre" ]; then
+			SERVER="whois.ripe.net"
+		elif [ "$ORG" == "African Network Information Center" ]; then
+			SERVER="whois.afrinic.net"
+		elif [ "$ORG" == "Latin American and Caribbean IP address Regional Registry" ]; then
+			SERVER="whois.lacnic.net"
+		fi
+		LOG "" "$DEST"
+		LOG "Whois Query ($SERVER) for: $SITE" "$DEST"
+		whois -h "$SERVER" "$SITE" | egrep -v "^$" | egrep -v "^#" >> "$DEST"
+		RV=$?
+	fi
 
 	END "$0" "$DEST"
 else
