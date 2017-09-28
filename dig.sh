29552674171d7eae6052169083d575a2bf81cf84
Matt Kucenski <mkucenski@me.com>
2017-09-28 08:40:05 -0600

    changing from ANY query which will not always get all data to individual queries of commonly interesting types; also cleaned up output to be more concise.

--- dig.sh
+++ dig.sh
@@ -3,10 +3,17 @@
 
 # Run dig/nslookup records consistently and store the results in a specific directory
 
+# NOTE:	Querying the DNS system for "ANY" is not reliable; hence individual queries for
+#			commonly interesting types. In some cases, "ANY" will not return certain records
+#			while a specific query for "MX" will return an entry.
+
 SITE="$1"
 DESTDIR="$2"
 if [ $# -eq 0 ]; then
 	USAGE "SITE" "DESTDIR" && exit $COMMON_ERROR
+fi
+if [ -z "$DESTDIR" ]; then
+	DESTDIR="./"
 fi
 
 RV=$COMMON_SUCCESS
@@ -22,8 +29,10 @@ if [ -e "$DEST" ]; then
 	INFO "$SITE -> $DEST"
 	LOG "Dig Query for: $SITE" "$DEST"
 
-	dig "$SITE" ANY | tee -a "$DEST"
-	RV=$((RV+$?))
+	for TYPE in A AAAA CNAME DNAME LOC MX NS PTR RP SOA SRV TXT URI; do
+		dig "$SITE" $TYPE | egrep -v "(;; Query time:|;; MSG SIZE|;; Got answer:|;; global options:|; <<>> DiG|;; ->>HEADER<<-|;; flags:|;; SERVER:|;; WHEN:)" | "$SEDCMD" -r '/^\s*$/d; s/(;; QUESTION SECTION:)/\n\1/' | tee -a "$DEST"
+		RV=$((RV+$?))
+	done
 
 	END "$0" "$DEST"
 else
