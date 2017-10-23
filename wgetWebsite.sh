45953cb665d28c6f8141b861b35e3acbb0dc28aa
Matt Kucenski <mkucenski@me.com>
2017-10-23 11:39:11 -0600

    initial round of updating to match scripting conventions

--- wgetWebsite.sh
+++ wgetWebsite.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
+. ${BASH_SOURCE%/*}/common-include.sh || exit 1
 
 SITE="$1"
-DEST="$2"
+DESTDIR="$2"
+if [ $# -eq 0 ]; then
+	USAGE "SITE" "DESTDIR" && exit $COMMON_ERROR
+fi
+
 LOG="./$SITE.log"
 
-mkdir -p "$DEST/$SITE"
-pushd "$DEST/$SITE"
+RV=$COMMON_SUCCESS
+START "$0" "$LOG" "$*"
+LOG_EXEC_VERSION "wget" "$(wget --version)" "$LOG"
 
-~/Scripts/whois.sh "$SITE" ./
+if [ ! -e "$DESTDIR/$SITE" ]; then
+	mkdir -p "$DESTDIR/$SITE"
+	pushd "$DESTDIR/$SITE"
 
-wget --recursive --level=1 --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --span-hosts --adjust-extension --page-requisites --server-response --convert-links --backup-converted "$SITE"
+	${BASH_SOURCE%/*}/whois.sh "$SITE" ./
+	${BASH_SOURCE%/*}/dig.sh "$SITE" ./
 
-echo "Script Code --------------------------------------------------" >> "$LOG"
-cat "$0" >> "$LOG"
-echo "Script Code --------------------------------------------------" >> "$LOG"
+	# wget --recursive --level=1 --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --span-hosts --adjust-extension --page-requisites --server-response --convert-links --backup-converted "$SITE"
+	wget --recursive --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --adjust-extension --page-requisites --server-response --convert-links --backup-converted "$SITE"
 
-echo "" >> "$LOG"
+	popd
+else
+	ERROR "Site directory ($DESTDIR/$SITE) already exists!" "$0" "$LOG"
+fi
 
-echo "Software Versions --------------------------------------------------" >> "$LOG"
-uname -v >> "$LOG"
-wget --version >> "$LOG"
-echo "Software Versions --------------------------------------------------" >> "$LOG"
-
-popd
+END "$0" "$LOG"
+exit $RV
 
