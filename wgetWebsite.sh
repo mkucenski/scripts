8456f695aaa463e63a27b6f932a8f6ba2d7836c9
Matt Kucenski <mkucenski@me.com>
2017-10-24 09:48:41 -0600

    Various changes to try and make these functional/usable.

--- wgetWebsite.sh
+++ wgetWebsite.sh
@@ -7,27 +7,35 @@ if [ $# -eq 0 ]; then
 	USAGE "SITE" "DESTDIR" && exit $COMMON_ERROR
 fi
 
-LOG="./$SITE.log"
+SITE_STRIPPED="$(echo "$SITE" | $SEDCMD -r -f "${BASH_SOURCE%/*}/sed/url-strip-to-filename.sed")"
 
 RV=$COMMON_SUCCESS
-START "$0" "$LOG" "$*"
-LOG_EXEC_VERSION "wget" "$(wget --version)" "$LOG"
 
-if [ ! -e "$DESTDIR/$SITE" ]; then
-	${BASH_SOURCE%/*}/whois.sh "$SITE" "$DESTDIR"
-	${BASH_SOURCE%/*}/dig.sh "$SITE" "$DESTDIR"
+if [ ! -e "$DESTDIR/$SITE_STRIPPED" ]; then
+	mkdir -p "$DESTDIR/$SITE_STRIPPED"
+	pushd "$DESTDIR/$SITE_STRIPPED"
 
-	mkdir -p "$DESTDIR/$SITE"
-	pushd "$DESTDIR/$SITE"
+	LOG="./$SITE_STRIPPED.log"
+	START "$0" "$LOG" "$*"
+	LOG_EXEC_VERSION "wget" "$(wget --version | grep "GNU Wget")" "$LOG"
+
+	# Save whois/dig records for specified site
+	${BASH_SOURCE%/*}/whois.sh "$SITE_STRIPPED" ./
+	${BASH_SOURCE%/*}/dig.sh "$SITE_STRIPPED" ./
 
 	# wget --recursive --level=1 --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --span-hosts --adjust-extension --page-requisites --server-response --convert-links --backup-converted "$SITE"
 	wget --recursive --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --adjust-extension --page-requisites --server-response --convert-links --backup-converted "$SITE"
 
 	popd
+
+	# Hash/archive results for retention
+	"${BASH_SOURCE%/*}/fciv_archive.sh" "$DESTDIR/$SITE_STRIPPED" 0 "$DESTDIR/$SITE_STRIPPED.7z"
+
+	END "$0" "$LOG"
 else
-	ERROR "Site directory ($DESTDIR/$SITE) already exists!" "$0" "$LOG"
+	ERROR "Site directory ($DESTDIR/$SITE_STRIPPED) already exists!" "$0"
+	RV=$COMMON_ERROR
 fi
 
-END "$0" "$LOG"
 exit $RV
 
