5347263bfb0e0e0a75418f6f5e21df2469b42054
Matt Kucenski <mkucenski@me.com>
2017-12-07 14:59:32 -0700

    rename/rework slightly

--- olecfexport.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-. "${BASH_SOURCE%/*}/common-include.sh" || exit 1
-
-MSG="$(FULL_PATH "$1")"
-
-olecfexport "$MSG"
-ln -s "$MSG.export/__substg1.0_64F00102/StreamData.bin" "$MSG.export/headers.txt"
-
