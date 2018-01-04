e57c2a813cb341088cd1e87c9727f2a793543936
Matt Kucenski <mkucenski@me.com>
2018-01-04 14:37:30 -0700

    'MIR' option too dangerous; causes deletion of files; don't use.

--- windows/robocopy.bat
+++ windows/robocopy.bat
@@ -6,7 +6,7 @@ echo Source: %src%
 echo Dest:   %dst%
 
 pushd %dst%
-robocopy /R:3 /W:10 /S /Z /J /MT:4 /MIR /TEE /LOG+:.\robocopy.log %src% %dst%
+robocopy /R:3 /W:10 /S /Z /J /MT:4 /E /TEE /LOG+:.\robocopy.log %src% %dst%
 popd
 
 pause
