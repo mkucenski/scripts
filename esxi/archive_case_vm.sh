95128848c8a9e7ee235388a07bf98fc3a6ca5ed0
Matt Kucenski <mkucenski@me.com>
2018-02-07 08:38:37 -0700

    Linking the HDDStore directory to the SSDStore directory and following symbolic links during the tar process.

--- esxi/archive_case_vm.sh
+++ esxi/archive_case_vm.sh
@@ -8,13 +8,17 @@ fi
 
 DATE=$(date "+%Y%m%d")
 FCIV="$(dirname "$0")/fciv_esxi.sh"
+LINK="$(dirname "$0")/link_hddstore.sh"
 ARCHIVE="$DESTDIR/$(basename "$VMDIR") ($DATE).tar.bz2"
+
+echo "Linking HDDStore directory to <$(basename "$VMDIR")/HDDStore>..."
+"$LINK" "$VMDIR"
 
 echo "Executing MD5 on all VM files..."
 "$FCIV" "$VMDIR"/* | tee "$VMDIR/fciv.md5"
 
 echo "Archiving all VM files..."
-tar cjvf "$ARCHIVE" "$VMDIR"
+tar cjvhf "$ARCHIVE" "$VMDIR"
 
 echo "Executing MD5 on archive file..."
 "$FCIV" "$ARCHIVE" | tee "$ARCHIVE.md5"
