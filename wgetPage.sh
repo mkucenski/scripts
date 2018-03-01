8853f90b74c5d3399ad3fb3cc2166e1b0edacba4
Matt Kucenski <mkucenski@me.com>
2018-03-01 06:25:38 -0700

    ...

--- wgetPage.sh
+++ wgetPage.sh
@@ -9,7 +9,7 @@ LOG="`basename $0`.log"
 mkdir -p "$DEST"
 pushd "$DEST"
 
-wget -e robots=off --span-hosts --wait 0.25 --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --adjust-extension --page-requisites --server-response --convert-links --backup-converted --recursive --level=5 "$URL"
+wget -e robots=off --span-hosts --wait 0.25 --append-output "$LOG" --show-progress -t 3 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17" --adjust-extension --page-requisites --server-response --convert-links --backup-converted --recursive --level=1 "$URL"
 
 echo "Script Code --------------------------------------------------" >> "$LOG"
 cat "$0" >> "$LOG"
@@ -20,7 +20,7 @@ echo "" >> "$LOG"
 echo "Software Versions --------------------------------------------------" >> "$LOG"
 uname -v >> "$LOG"
 wget --version >> "$LOG"
-torify --version >> "$LOG"
+# torify --version >> "$LOG"
 echo "Software Versions --------------------------------------------------" >> "$LOG"
 
 popd
