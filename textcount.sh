0db96eb03b34fb8ebc79cf4690b2809098acaf54
Matt Kucenski <mkucenski@me.com>
2018-08-02 16:01:41 -0600

    ...

--- textcount.sh
+++ textcount.sh
@@ -7,10 +7,16 @@ if [ $# -eq 0 ]; then
 fi
 
 if [ -e "$FILE" ]; then
+	TEMP=$(MKTEMP "$0" || exit 1)
+
 	echo "$FILE:"
 	for TERM in $@; do
 		COUNT=$(grep -i "$TERM" "$FILE" | cat -n | tail -n 1 | cut -f 1 | gsed -r 's/[[:space:]]+//g')
-		echo "$TERM: $COUNT"
+		echo "$TERM:	$COUNT" >> "$TEMP"
 	done
+
+	sort --numeric-sort --reverse --field-separator=":" --key=2 "$TEMP"
+
+	rm "$TEMP"
 fi
 
