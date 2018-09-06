5afc5e81b5e2163c8772eb4ca2c058805d8d4cfb
Matt Kucenski <mkucenski@me.com>
2018-09-06 09:58:09 -0600

    init for cleaning random port leftovers...

---
+++ macOS/macports-clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. "${BASH_SOURCE%/*}/../common-include.sh" || exit 1
+
+if [ $(CHECK_ROOT) != true ]; then
+	ERROR "This script *MUST* be run as 'root'!" && exit 1
+fi
+
+${BASH_SOURCE%/*}/macOS/macports-wrapper.sh -f clean --all all
+${BASH_SOURCE%/*}/macOS/macports-wrapper.sh -f uninstall inactive
+
