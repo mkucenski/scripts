b532d42f560cad9335a5863093a44c75c745ae05
Matt Kucenski <mkucenski@me.com>
2018-12-10 15:46:07 -0700

    ...

--- tsk/tsk-sid-mct-update.sh
+++ tsk/tsk-sid-mct-update.sh
@@ -7,24 +7,25 @@ MCT_GZ="$2"
 
 if [ $# -eq 0 ]; then
 	USAGE "SECID_SID_CSV" "MCT.GZ" && exit 1
+	USAGE_DESCRIPTION "This script uses the CSV output from 'tsk-sid-multi-lookup.sh' to generate new MCT data that includes the mapped SID values. It assumes you have already extracted the MCT data via tsk_gettimes (or other) and stored them in a gzip compressed file."
 fi
+
 IFS=$(echo -en "\n\b")
 
 SID_LIST="$(MKTEMP "$0" || exit 1)"
 cat "$SECID_SID_CSV" | cut -d "," -f 2 | gsed -r 's/[[:space:]]+//g;' | sort -u | egrep -v "^(0|SID|S-1-5-18|S-1-5-32-544)$" > "$SID_LIST"
 
-NAME="$(echo "$MCT_GZ" | gsed -r 's/\.mct\.gz$//')"
+NAME="$(echo "$MCT_GZ" | $SEDCMD -r 's/\.mct\.gz$//')"
 
 while read -r SID; do
 	REGEX="$(MKTEMP "$0.regex" || exit 1)"
-	MCT="$NAME-$SID.mct.gz"
 
 	echo -n "$SID:"
 	for SECID in $(grep -i "$SID" "$SECID_SID_CSV" | cut -d "," -f 1); do
 		echo -n " $SECID"
 		echo "^[^|]*\|[^|]+\|[^|]+\|[^|]*\|$SECID\|[^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*$" >> "$REGEX"
 	done
-	gunzip -c "$MCT_GZ" | egrep -f "$REGEX" | gsed -r "s/^([^|]*\|[^|]+\|[^|]+\|[^|]*)\|[^|]*\|([^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*)$/\1|$SID|\2/" | gzip -c > "$MCT"
+	gunzip -c "$MCT_GZ" | egrep -f "$REGEX" | gsed -r "s/^([^|]*\|[^|]+\|[^|]+\|[^|]*)\|[^|]*\|([^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*)$/\1|$SID|\2/" | gzip -c > "$NAME-$SID.mct.gz"
 	echo
 
 	rm "$REGEX"
--- tsk/tsk-sid-multi-lookup.sh
+++ tsk/tsk-sid-multi-lookup.sh
@@ -4,15 +4,20 @@
 
 IMAGE="$1"
 OFFSET="$2"
-SECID_LIST="$3"
-MCT_GZ="$4"
-CSV="$5"
+MCT_GZ="$3"
+
 if [ $# -eq 0 ]; then
-	USAGE "IMAGE" "OFFSET" "SECID_LIST" "MCT.GZ" && exit 1
+	USAGE "IMAGE" "OFFSET" "MCT.GZ" && exit 1
+	USAGE_DESCRIPTION "This script is a quick/dirty way to build a CSV mapping of 'Security IDs' to NTFS SID values for a given volume. It relies on a modified version of TSK that outputs the Security ID in the MCT owner record (TSK defaults to '0'). It assumes you have already extracted the MCT data via tsk_gettimes (or other) and stored them in a gzip compressed file."
 fi
 
 IFS=$(echo -en "\n\b")
 
+# Find all unique Security IDs found in the MCT data
+SECID_LIST="$(MKTEMP "$0")"
+gunzip -c "$MCT_GZ" | cut -d "|" -f 5 | sort -un > "$SECID_LIST"
+
+# Search the MCT data for each Security ID and retrieve it's inode; execute 'istat' to recover the SID value.
 echo "Security ID,SID"
 while read -r SECID; do
 	SECID_MCT="$(gunzip -c "$MCT_GZ" | egrep "^[^|]*\|[^|]+\|[^|]+\|[^|]*\|$SECID\|[^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*\|[^|]*$" | grep -v '$FILE_NAME' | head -n 1)"
@@ -21,4 +26,6 @@ while read -r SECID; do
 	SID="$(_tsk_istat_sid "$SECID_ISTAT" | cut -d " " -f 3)"
 	echo "$SECID,$SID"
 done < "$SECID_LIST"
+
+rm "$SECID_LIST"
 
