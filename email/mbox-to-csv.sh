425a24f533d69c371f5d171bf5427c22a65ecf03
Matt Kucenski <mkucenski@me.com>
2019-08-26 10:26:32 -0600

    Scripts to convert EML -> MBOX -> CSV for review/analysis.

--- email/mbox-to-csv.sh
+++ email/mbox-to-csv.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
-. "${BASH_SOURCE%/*}/../common-include.sh" || exit 1
+. "${BASH_SOURCE%/*}/common-include.sh" || exit 1
 
-EML="$1"
 if [ $# -eq 0 ]; then
-	USAGE "EML" && exit 1
+	USAGE "MBOX(S)"
+fi
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	USAGE_DESCRIPTION "This is a simple script to parse basic headers out of text-based email files (e.g. mbox format containing a single email) and output them in CSV format for quick review/analysis."
+	USAGE_EXAMPLE "$(basename "$0") mbox1 mbox2 mbox3 ..."
+	exit 1
 fi
 
-DATE="$(egrep -h "^Date:" "$EML" | head -n 1 | gsed -r 's/^Date://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//; s/ \+[[:digit:]]+$//; s/^[[:alpha:]]+, //')"
-SUBJECT="$(egrep -h "^Subject:" "$EML" | head -n 1 | gsed -r 's/^Subject://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
-FROM="$(egrep -h "^From:" "$EML" | head -n 1 | gsed -r 's/^From://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
-TO="$(egrep -h "^To:" "$EML" | head -n 1 | gsed -r 's/^To://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
-CC="$(egrep -h "^Cc:" "$EML" | head -n 1 | gsed -r 's/^Cc://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
-MESSAGEID="$(egrep -h "^Message-Id:" "$EML" | head -n 1 | gsed -r 's/^Message-Id://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
+echo "Date, Subject, From, To, CC, MessageID"
+for MBOX in $@; do
+	DATE="$(egrep -h "^Date:" "$MBOX" | head -n 1 | gsed -r 's/^Date://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//; s/ \+[[:digit:]]+$//; s/^[[:alpha:]]+, //')"
+	SUBJECT="$(egrep -h "^Subject:" "$MBOX" | head -n 1 | gsed -r 's/^Subject://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
+	FROM="$(egrep -h "^From:" "$MBOX" | head -n 1 | gsed -r 's/^From://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
+	TO="$(egrep -h "^To:" "$MBOX" | head -n 1 | gsed -r 's/^To://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
+	CC="$(egrep -h "^Cc:" "$MBOX" | head -n 1 | gsed -r 's/^Cc://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
+	MESSAGEID="$(egrep -h "^Message-Id:" "$MBOX" | head -n 1 | gsed -r 's/^Message-Id://; s/"//g; s/^[[:space:]]+//; s/[\r\n[:space:]]+$//')"
 
-echo "\"$DATE\",\"$SUBJECT\",\"$FROM\",\"$TO\",\"$CC\",\"$MESSAGEID\""
+	echo "\"$DATE\",\"$SUBJECT\",\"$FROM\",\"$TO\",\"$CC\",\"$MESSAGEID\""
+done
+
--- email/msg-to-mbox.sh
+++ email/msg-to-mbox.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
-. "${BASH_SOURCE%/*}/../common-include.sh" || exit 1
+. "${BASH_SOURCE%/*}/common-include.sh" || exit 1
 
-DEST="$1"; shift
+DEST="$1"
 if [ $# -eq 0 ]; then
-	USAGE "DEST EML_FILE(S)" && exit 1
+	USAGE "DEST EML_FILE(S)"
+	exit 1
+fi
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	USAGE_DESCRIPTION "This script converts multiple Outlook (EML) files into mbox format for easier parsing/analysis. Relies on: 'msgconvert' from 'p5.26-email-outlook-message' (https://metacpan.org/pod/Email::Outlook::Message)"
+	USAGE_EXAMPLE "$(basename "$0") eml1 eml2 eml3 ..." 
+	exit 1
 fi
 
-# ls ../*.msg | xargs -I {} msgconvert --mbox ./{}.eml {}
+LOGFILE="$DEST/$(STRIP_EXTENSION "$(basename "$0")").log"
+START "$0" "$LOGFILE" "$@"
+
+shift
 for X in $@; do
-	echo "$X:"
-	msgconvert --mbox "$(STRIP_EXTENSION "$X").mbox" "$X"
+	FILE="$DEST/$(STRIP_EXTENSION "$X").mbox"
+	INFO "$X -> $FILE" "$LOGFILE"
+	EXEC_CMD "msgconvert --mbox \"$FILE\" \"$X\"" "$LOGFILE"
+	EXEC_CMD "dos2unix \"$FILE\"" "$LOGFILE"
 done
+
+END "$0" "$LOGFILE"
+
