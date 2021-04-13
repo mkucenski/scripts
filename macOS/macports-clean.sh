e86fff72dc2fcb9ce0779fdfe6899358c8da265c
Matt Kucenski <mkucenski@users.noreply.github.com>
2021-04-13 09:35:55 -0700

    ...

--- macOS/macports-clean.sh
+++ macOS/macports-clean.sh
@@ -8,3 +8,12 @@ fi
 ${BASH_SOURCE%/*}/macports-wrapper.sh -f clean --all all
 ${BASH_SOURCE%/*}/macports-wrapper.sh -f uninstall inactive
 
+# These should be done automatically by the commands above:
+# sudo rm -rf /opt/local/var/macports/build/*
+# sudo rm -rf /opt/local/var/macports/distfiles/*
+# sudo rm -rf /opt/local/var/macports/packages/*
+
+ls /opt/local/var/macports/build/
+ls /opt/local/var/macports/distfiles/
+ls /opt/local/var/macports/packages/
+
