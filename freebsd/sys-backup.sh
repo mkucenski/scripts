7936ff338b6288cc504b889a1d14971d8172c99c
Matt Kucenski <mkucenski@users.noreply.github.com>
2022-02-25 15:38:05 -0800

    cleanup + storing alternate versions of configuration files to rebuild a system

--- freebsd/sys-backup.sh
+++ freebsd/sys-backup.sh
@@ -7,8 +7,13 @@ if [ $# -eq 0 ]; then
 fi
 
 if [ -d "$DESTDIR" ]; then
-	DEST="$DESTDIR/$(hostname -s) $(DATETIME).bz2"
-	dump -L0af - / | bzip2 -c > "$DEST"
+	HOSTNAME=$(hostname -s)
+	TIMESTAMP=$(DATETIME)
+	DESTPREFIX="$DESTDIR/$HOSTNAME $TIMESTAMP"
+
+	dump -L0af - / | bzip2 -c > "$DESTPREFIX dump.bz2"
+	pkg prime-list > "$DESTPREFIX pkgs.txt""
+	tar cjvf "$DESTPREFIX.tar.bz2" "/boot/loader.conf" /boot/loader.conf.local" "/etc" "/usr/local/etc"
 else
 	ERROR "Invalid Destination Directory!" "$0" && exit 1
 fi
